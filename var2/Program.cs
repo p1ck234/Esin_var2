7700777697afa7fb6ebd4c0ebce3e5d27f2d5012
Danil <93711640+p1ck234@users.noreply.github.com>
2022-12-09 17:47:32 +0400

    upl

---
+++ var2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int max;
+
+            List<int> list = new List<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(-1);
+            list.Add(4);
+            list.Add(4);
+            int counter = 0;
+            void MAX(List<int> _list)
+            {
+                max = list.Max();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] == max)
+                    {
+                        counter = i;
+                    }
+                }
+                list[counter] = x;
+            }
+
+            MAX(list);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
